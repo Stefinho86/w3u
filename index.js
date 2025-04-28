883e3c29c300edcaa807d74da9cf4d12061652d6
Stefinho86 <stefano.casaretta@hotmail.it>
2025-04-28 16:43:55 +0200

    Aggiornato index.js per scarico file .w3u

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const JSON_URL = "https://pez8.up.railway.app/";
 
 app.get("/", (req, res) => {
-  res.send("Ciao! Vai su /playlist.w3u per ottenere la playlist in JSON identica all’originale.");
+  res.send("Vai su /playlist.w3u per scaricare il file originale in formato JSON.");
 });
 
 app.get("/playlist.w3u", async (req, res) => {
@@ -13,9 +13,10 @@ app.get("/playlist.w3u", async (req, res) => {
     const response = await fetch(JSON_URL);
     const json = await response.json();
 
+    // Header per scaricare come file .w3u (ma formato JSON puro!)
     res.setHeader("Content-Type", "application/json; charset=utf-8");
     res.setHeader("Content-Disposition", "attachment; filename=playlist.w3u");
-    res.send(JSON.stringify(json, null, 2)); // Indenta per leggibilità (opzionale)
+    res.send(JSON.stringify(json, null, 2)); // Formattazione leggibile, opzionale
   } catch (err) {
     res.status(500).send("Errore nel recupero del JSON");
   }
@@ -23,5 +24,5 @@ app.get("/playlist.w3u", async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log("Server avviato sulla porta " + PORT);
+  console.log(`Server avviato sulla porta ${PORT}`);
 });
